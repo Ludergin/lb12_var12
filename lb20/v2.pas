c4007da4220e7b74aaf6fa44f1498130840e1f8a
Maksim <156791693+Ludergin@users.noreply.github.com>
2024-02-25 18:06:23 +1000

    Update v2.pas

--- lb20/v2.pas
+++ lb20/v2.pas
@@ -6,7 +6,7 @@ begin
   median := sqrt(2 * (b * b + c * c) - a * a) / 2;
 end;
 
-function func1(a,b,c:real):real;
+function f(a,b,c:real):real;
 var
   med_a,med_b,med_c,med:real;
 begin
@@ -14,36 +14,14 @@ begin
   proc(b,c,a,med_b);
   proc(c,a,b,med_c);
   proc(med_a,med_b,med_c,med);
-  func1:=med;
-end;
-
-function func2(a,b,c:real):real;
-var
-  med_a,med_b,med_c,med:real;
-begin
-  proc(a,b,c,med_a);
-  proc(b,c,a,med_b);
-  proc(c,a,b,med_c);
-  proc(med_a,med_b,med_c,med);
-  func2:=med;
-end;
-
-function func3(a,b,c:real):real;
-var
-  med_a,med_b,med_c,med:real;
-begin
-  proc(a,b,c,med_a);
-  proc(b,c,a,med_b);
-  proc(c,a,b,med_c);
-  proc(med_a,med_b,med_c,med);
-  func3:=med;
+  f:=med;
 end;
 
 function func(a,b,c:real):real;
 begin
-  writeln(func1(a,b,c));
-  writeln(func2(a,b,c));
-  writeln(func3(a,b,c));
+  writeln(f(a,b,c));
+  writeln(f(c,a,b));
+  writeln(f(b,c,a));
 end;
 
 begin
